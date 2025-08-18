5504dd05b43fb24831392192bd2a7a696c33a51e
Abdullah-Naeem <108383288+El3ziiiz@users.noreply.github.com>
2025-08-18 04:25:17 +0300

    Create ListView.swift

---
+++ ListView.swift
@@ -0,0 +1,53 @@
+//
+//  ListView.swift
+//  Moonshot
+//
+//  Created by عبدالله on 15/08/2025.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+        var body: some View {
+        ScrollView{
+            LazyVStack() {
+                ForEach(missions) { mission in
+                    NavigationLink(value: mission){
+                        VStack{
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            VStack{
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(.rect(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    return ListView(astronauts: astronauts, missions: missions)
+}
